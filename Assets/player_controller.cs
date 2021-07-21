c13e58cbb3aaa0fa3fe3c10a1bea911a81bae437
Newturn <rahul_i86@yahoo.com>
2021-07-21 16:52:56 +0530

    Crouch and jump

--- Assets/player_controller.cs
+++ Assets/player_controller.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class player_controller : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
 
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    
+    //}
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float translation = Input.GetAxis("Vertical") * speed;
         animator.SetFloat("speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
 
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
 
+
+            //Send the message to the Animator to activate the trigger parameter named "Crouch"
+            animator.SetTrigger("Crouch");
+        }
+        else
+        {
+            animator.ResetTrigger("Crouch");
+        }
         if (speed < 0)
         {
-            scale.x = -1f * Mathf.Abs(scale.x);
+            scale.x = -1f * scale.x;
 
 
         }
@@ -25,14 +42,11 @@ public class player_controller : MonoBehaviour
         else if (speed > 0)
         {
             scale.x = Mathf.Abs(scale.x);
+            animator.SetTrigger("Jump");
+
         }
 
         transform.localScale = scale;
-
-
-
-
-
 
     }
 }
